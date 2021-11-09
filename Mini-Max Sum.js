6bd4e3b5fa3f9d93f428b5b47dff9e82893717a6
JoshuaJDevine <jd8386@gmail.com>
2021-11-09 09:23:28 -0800

    Mini-Max Sum

---
+++ Mini-Max Sum.js
@@ -0,0 +1,109 @@
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+Explanation
+
+The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Hints: Beware of integer overflow! Use 64-bit Integer.
+ */
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    /*
+    Given the input restraints,
+    sorting the array means the first four digits will always be the smallest sum,
+    the last four elements will always be the largest sum.
+    */
+
+
+    //Sort the array
+    let sortedArr = arr.sort();
+
+
+    //Get the required data sets from the sorted array
+    let maxSet = arr.slice(1);
+    let minSet = arr.slice(0, 4);
+
+
+    //use the built in reduce function to sum the array
+    let minSum = minSet.reduce((a,b) => a+b, 0)
+    let maxSum = maxSet.reduce((a,b) => a+b, 0)
+
+
+    //No return value, expected out is a space separated console log
+    console.log(minSum, maxSum)
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
