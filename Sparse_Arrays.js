b616a63b9e08895bed1528d35f05e2b99f2857c8
JoshuaJDevine <jd8386@gmail.com>
2021-11-10 10:41:40 -0800

    Sprse Arrays

---
+++ Sparse_Arrays.js
@@ -0,0 +1,112 @@
+/*
+There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+
+Example
+
+
+
+There are  instances of ',  of '' and  of ''. For each query, add an element to the return array, .
+
+Function Description
+
+Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in strings.
+
+matchingStrings has the following parameters:
+
+string strings[n] - an array of strings to search
+string queries[q] - an array of query strings
+Returns
+
+int[q]: an array of results for each query
+Input Format
+
+The first line contains and integer , the size of .
+Each of the next  lines contains a string .
+The next line contains , the size of .
+Each of the next  lines contains a string .
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    //Returning an array that describes the number of occurrences of query in strings
+    let res = []
+
+
+    //Check all queries
+    queries.forEach((q) => {
+        //Track occurrences
+        let occurrences = 0;
+        strings.forEach((s) => {
+            //If q === string increment occurrences
+            if (q === s) occurrences ++;
+        })
+        //Push occurrences
+        res.push(occurrences);
+    })
+
+
+    //Return Occurrences
+    return res;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine().trim(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(strings, queries);
+
+    ws.write(res.join('\n') + '\n');
+
+    ws.end();
+}
+
