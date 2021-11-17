d2f0f1237140ecb8ff0355f0760c24a6936310d3
JoshuaJDevine <jd8386@gmail.com>
2021-11-17 10:11:05 -0800

    Pangram Complete

---
+++ Panagram.js
@@ -0,0 +1,105 @@
+/*
+https://www.hackerrank.com/challenges/one-month-preparation-kit-pangrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+
+Example
+
+The string contains all letters in the English alphabet, so return pangram.
+
+Function Description
+
+Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+
+pangrams has the following parameter(s):
+
+string s: a string to test
+Returns
+
+string: either pangram or not pangram
+Input Format
+
+A single line with string .
+
+Constraints
+
+
+Each character of ,
+
+Sample Input
+
+Sample Input 0
+
+We promptly judged antique ivory buckles for the next prize
+
+Sample Output 0
+
+pangram
+
+Sample Explanation 0
+
+All of the letters of the alphabet are present in the string.
+
+Sample Input 1
+
+We promptly judged antique ivory buckles for the prize
+
+Sample Output 1
+
+not pangram
+
+Sample Explanation 0
+
+The string lacks an x.
+ */
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    let alphabet = new Set("abcdefghijklmnopqrstuvwxyz")
+
+    for (let c of s.toLowerCase()){
+        alphabet.delete(c);
+        if (alphabet.size === 0) return "pangram";
+    }
+    return "not pangram";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = pangrams(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
