4d73f5986a547d980bb321d3c0ce28d71297d2a9
JoshuaJDevine <jd8386@gmail.com>
2022-01-17 11:20:48 -0800

    Complete Caesar Cipher, unoptomized

--- Caesar Cipher.js
+++ Caesar Cipher.js
@@ -1,4 +1,43 @@
 function caesarCipher(s, k) {
-    // Write your code here
+    if (s === "") return s;
+    if (k === 0) return s;
+    let alphapbetLower = "abcdefghijklmnopqrstuvwxyz";
+    let alphapbetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let newS = "";
 
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        let idx = i;
+
+        if (alphapbetLower.indexOf(char.toLowerCase()) == -1) {
+            newS += char;
+            continue;
+        }
+        else {
+            idx = alphapbetLower.indexOf(char.toLowerCase());
+        }
+        if (idx+k >= 26)
+        {
+            idx = (idx + k) % 26;
+            console.log("New IDX")
+            let isLower = alphapbetLower.indexOf(char) > -1
+
+            if (isLower)
+                newS += alphapbetLower[idx];
+            else
+                newS += alphapbetUpper[idx];
+        }
+        else {
+            let isLower = alphapbetLower.indexOf(char) > -1
+
+            if (isLower)
+                newS += alphapbetLower[idx+k];
+            else
+                newS += alphapbetUpper[idx+k];
+        }
+    }
+
+    return newS;
 }
+
+console.log(caesarCipher("Hello_World!", 4))
