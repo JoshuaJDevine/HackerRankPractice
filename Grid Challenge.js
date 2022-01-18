6448e098c02828e64b70db53aa5cc24c27413fa9
JoshuaJDevine <jd8386@gmail.com>
2022-01-18 12:05:58 -0800

    Unoptomized Solution

--- Grid Challenge.js
+++ Grid Challenge.js
@@ -1,58 +1,28 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'gridChallenge' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING_ARRAY grid as parameter.
- */
-
 function gridChallenge(grid) {
-    // Write your code here
+    //Sort Grid using built in functions
+    for(let i = 0; i < grid.length; i++)
+    {
+        grid[i] = grid[i].split('');
 
-}
+        grid[i].sort();
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const t = parseInt(readLine().trim(), 10);
-
-    for (let tItr = 0; tItr < t; tItr++) {
-        const n = parseInt(readLine().trim(), 10);
-
-        let grid = [];
-
-        for (let i = 0; i < n; i++) {
-            const gridItem = readLine();
-            grid.push(gridItem);
-        }
-
-        const result = gridChallenge(grid);
-
-        ws.write(result + '\n');
+        grid[i].join('');
     }
 
-    ws.end();
+    //Check challenge conditions
+    for(let i = 0; i < grid[0].length; i++)
+    {
+        let char = grid[0][i];
+
+        for(let j = 1; j < grid.length; j++)
+        {
+            if(grid[j][i] < char)
+            {
+                return "NO";
+            }
+        }
+    }
+    return "YES";
 }
+
+console.log(gridChallenge(grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']))
